2b79744a9388dbfa3adae609bda64ffa2b226ab6
biansetuliao <TMWQ@tmwq.deskxd.org>
2015-10-08 18:14:35 +0800

    create mouse wheel js

--- Turanga_deskxd_com/urls.py
+++ Turanga_deskxd_com/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import include, url
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('home.urls', namespace="home")),
+    url(r'^download', include('download.urls', namespace="download")),
+    url(r'^about', include('about.urls', namespace="about")),
 ]
---
+++ static/assets/js/mouse/main.js
@@ -0,0 +1,27 @@
+var system = {
+	win: false,
+	mac: false,
+	xll: false,
+	ipad:false
+};
+//检测平台
+var p = navigator.platform;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+//跳转语句
+if (system.win || system.mac || system.xll) {
+	$(function () {
+		$(".panel").css({"height": $(window).height()});
+		$.scrollify({
+			section: ".panel"
+		});
+
+
+		$(".scroll").click(function (e) {
+			e.preventDefault();
+			$.scrollify("move", $(this).attr("href"));
+		});
+	});
+}
