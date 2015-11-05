769cb4fd7d00c02d210e1c7095cdf265fd6f4814
涂缪文青 <biansetuliao@gmail.com>
2015-11-05 18:39:36 +0800

    update beta

--- testing/urls.py
+++ testing/urls.py
@@ -3,4 +3,10 @@ from testing import views
 
 urlpatterns = [
     url(r'^$', views.IndexViews.as_view(), name='index'),
+
+    url(r'^success', views.SuccessViews.as_view(), name='success'),
+
+    url(r'^sendmessage', views.sendmessage, name='sendmessage'),
+
+    url(r'^beta', views.BetaViews.as_view(), name='beta'),
 ]
--- testing/views.py
+++ testing/views.py
@@ -1,7 +1,13 @@
+#encoding=utf8
 from django.shortcuts import render
 from django.views import generic
 from models import *
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail, EmailMultiAlternatives, get_connection
+
+
+from models import *
+import re
 
 class IndexViews(generic.View):
     templates_file = 'testing.html'
@@ -14,3 +20,84 @@ class IndexViews(generic.View):
         return render(request,
                       self.templates_file,
                       context)
+
+
+class SuccessViews(generic.View):
+    templates_file = 'success.html'
+
+    def get(self, request):
+
+        error_num = request.GET['error']
+
+        context = {
+            "error_num": error_num,
+        }
+
+        return render(request,
+                      self.templates_file,
+                      context)
+
+
+class SuccessViews(generic.View):
+    templates_file = 'success.html'
+
+    def get(self, request):
+
+        error_num = request.GET['error']
+
+        context = {
+            "error_num": error_num,
+        }
+
+        return render(request,
+                      self.templates_file,
+                      context)
+
+class BetaViews(generic.View):
+    templates_file = 'beta.html'
+
+    def get(self, request):
+
+        mobile_name = request.GET['mobile']
+
+        context = {
+            "mobile_name": mobile_name,
+        }
+
+        return render(request,
+                      self.templates_file,
+                      context)
+
+
+def sendmessage(request):
+
+    if 'email' in request.POST and request.POST['email']:
+        email = request.POST['email']
+    else:
+        return HttpResponseRedirect('/beta/success?error=%d' % int(2))
+
+    isemail = re.match(r'[A-Za-z0-9][\w\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+$', email)
+    if isemail:
+        users = list(UserMessage.objects.filter(email=email))
+        if users:
+            return HttpResponseRedirect('/beta/success?error=%d' % int(0))
+        else:
+            p = UserMessage(
+                email=email
+            )
+            p.save()
+
+            # # send-email
+            # mail_list = ["784184594@qq.com", ]
+            # send_mail(
+            #     subject='新增用户邮箱',
+            #     message=p.email,
+            #     from_email='tumiaowenqing@live.cn',
+            #     recipient_list=mail_list,
+            #     fail_silently=False
+            # )
+
+    else:
+        return HttpResponseRedirect('/beta/success?error=%d' % int(2))
+
+    return HttpResponseRedirect('/beta/success?error=%d' % int(1))
