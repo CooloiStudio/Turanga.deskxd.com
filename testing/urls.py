7c3b89d5eabac8425d20ea635149d8d11a3ee6cd
涂缪文青 <biansetuliao@gmail.com>
2015-11-06 11:18:35 +0800

    update

--- testing/urls.py
+++ testing/urls.py
@@ -4,11 +4,11 @@ from testing import views
 urlpatterns = [
     url(r'^$', views.IndexViews.as_view(), name='index'),
 
-    url(r'^success', views.SuccessViews.as_view(), name='success'),
+    url(r'^success/', views.SuccessViews.as_view(), name='success'),
 
-    url(r'^sendmessage', views.sendmessage, name='sendmessage'),
+    url(r'^sendmessage/', views.sendmessage, name='sendmessage'),
 
-    url(r'^ios', views.IOSViews.as_view(), name='ios'),
+    url(r'^ios/', views.IOSViews.as_view(), name='ios'),
 
-    url(r'^android', views.ANDROIDViews.as_view(), name='android'),
+    url(r'^android/', views.ANDROIDViews.as_view(), name='android'),
 ]
