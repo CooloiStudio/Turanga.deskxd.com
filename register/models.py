c7bc7f218ed830494350e74a2fdb5d31ad7d2b25
涂缪文青 <biansetuliao@gmail.com>
2015-11-11 18:29:10 +0800

    update

--- register/models.py
+++ register/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from DjangoUeditor.models import UEditorField
 
 class Agreement(models.Model):
-    content = UEditorField(u'test',width=1200, height=600, toolbars="full", imagePath="", filePath="", upload_settings={"imageMaxSize":1204000}, settings={}, command=None, blank=True)
+    content = UEditorField(u'test',width=1200, height=600, toolbars="full", settings={}, command=None, blank=True)
