331c0330cacb8205ced75d96eb7b83a5c7ecef38
涂缪文青 <biansetuliao@gmail.com>
2015-11-13 11:05:16 +0800

    update

--- Turanga_deskxd_com/urls.py
+++ Turanga_deskxd_com/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('home.urls', namespace="home")),
     url(r'^thanks/', include('thanks.urls', namespace="thanks")),
     url(r'^about/', include('about.urls', namespace="about")),
-    url(r'^register/', include('register.urls', namespace="register")),
-    url(r'^beta/', include('testing.urls', namespace="beta")),
-    url(r'^ueditor/',include('DjangoUeditor.urls')),
+    # url(r'^register/', include('register.urls', namespace="register")),
+    # url(r'^beta/', include('testing.urls', namespace="beta")),
+    # url(r'^ueditor/',include('DjangoUeditor.urls')),
 ]
