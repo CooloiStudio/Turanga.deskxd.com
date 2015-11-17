5354e9c1454bd89608273ef7dfd51110f44b8ed9
涂缪文青 <biansetuliao@gmail.com>
2015-11-17 09:46:11 +0800

    update

--- Turanga_deskxd_com/urls.py
+++ Turanga_deskxd_com/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('home.urls', namespace="home")),
     url(r'^thanks/', include('thanks.urls', namespace="thanks")),
-    url(r'^about/', include('about.urls', namespace="about")),
-    url(r'^register/', include('register.urls', namespace="register")),
+    # url(r'^about/', include('about.urls', namespace="about")),
+    # url(r'^register/', include('register.urls', namespace="register")),
     url(r'^beta/', include('testing.urls', namespace="beta")),
     # url(r'^ueditor/',include('DjangoUeditor.urls')),
 ]
