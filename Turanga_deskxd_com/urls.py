64d7a5e3128c85d1d3be38d36913d79b16cff537
涂缪文青 <biansetuliao@gmail.com>
2015-12-30 19:39:00 +0800

    update urls.py admin to launchpad

--- Turanga_deskxd_com/urls.py
+++ Turanga_deskxd_com/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^launchpad/', include(admin.site.urls)),
     url(r'^$', include('home.urls', namespace="home")),
     url(r'^thanks/', include('thanks.urls', namespace="thanks")),
     # url(r'^about/', include('about.urls', namespace="about")),
